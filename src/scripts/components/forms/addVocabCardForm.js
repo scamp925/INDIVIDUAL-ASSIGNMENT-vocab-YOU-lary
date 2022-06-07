b19bd5f7a9e53c6ad65edae3e2a36e2061692b6d
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-06 19:56:40 -0500

    Update form to be used for either a new card or to update an existing card

--- src/scripts/components/forms/addVocabCardForm.js
+++ src/scripts/components/forms/addVocabCardForm.js
@@ -1,21 +1,21 @@
 import clearDom from '../../helpers/clearDom';
 import renderToDom from '../../helpers/renderToDom';
 
-const addVocabCardForm = () => {
+const addVocabCardForm = (obj = {}) => {
   clearDom();
-  const domString = `<h2>Add New Word</h2>
-  <form>
+  const domString = `
+  <form id="${obj.firebaseKey ? `update-vocab-word--${obj.firebaseKey}` : 'submit-vocab-word'}">
   <div class="mb-3">
   <label for="title" class="form-label">Word or Phrase</label>
-  <input type="text" class="form-control" id="title" placeholder="name@example.com">
+  <input type="text" class="form-control" id="title" placeholder="Your Word Here" value="${obj.title || ''}" required>
 </div>
 <div class="mb-3">
   <label for="definition" class="form-label">Definition</label>
-  <textarea class="form-control" id="definition" rows="3" placeholder="name@example.com"></textarea>
+  <textarea class="form-control" id="definition" rows="3" placeholder="Enter Definition" value="${obj.definition || ''} required></textarea>
 </div>
 <div class="mb-3">
   <label for="title" class="form-label">Category</label>
-  <input type="text" class="form-control" id="title" placeholder="Enter the coding language that best fits your new word">
+  <input type="text" class="form-control" id="category" placeholder="Enter the coding language that best fits your new word" value="${obj.category || ''}" required>
 </div>
 <button type="submit" class="btn btn-success">Submit</button>
 </form>`;
