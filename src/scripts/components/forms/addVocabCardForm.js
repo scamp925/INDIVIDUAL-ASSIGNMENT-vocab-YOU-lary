d3b0d424a3bd4fed6651576865e141e5a730c0c8
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-06 20:11:12 -0500

    Changed form for new vocab card. Once 'Create a New Word' is clicked, the intended form renders

--- src/scripts/components/forms/addVocabCardForm.js
+++ src/scripts/components/forms/addVocabCardForm.js
@@ -4,20 +4,21 @@ import renderToDom from '../../helpers/renderToDom';
 const addVocabCardForm = (obj = {}) => {
   clearDom();
   const domString = `
-  <form id="${obj.firebaseKey ? `update-vocab-word--${obj.firebaseKey}` : 'submit-vocab-word'}">
-  <div class="mb-3">
-  <label for="title" class="form-label">Word or Phrase</label>
-  <input type="text" class="form-control" id="title" placeholder="Your Word Here" value="${obj.title || ''}" required>
-</div>
-<div class="mb-3">
-  <label for="definition" class="form-label">Definition</label>
-  <textarea class="form-control" id="definition" rows="3" placeholder="Enter Definition" value="${obj.definition || ''} required></textarea>
-</div>
-<div class="mb-3">
-  <label for="title" class="form-label">Category</label>
-  <input type="text" class="form-control" id="category" placeholder="Enter the coding language that best fits your new word" value="${obj.category || ''}" required>
-</div>
-<button type="submit" class="btn btn-success">Submit</button>
+  <form id="${obj.firebaseKey ? `update-vocab-word--${obj.firebaseKey}` : 'submit-vocab-word'}" class="mb-4">
+  <div class="form-group">
+        <label for="word">Word or Phrase</label>
+        <input type="text" class="form-control" id="word" aria-describedby="vocab-work" placeholder="Enter Your Word or Phrase Here" value="${obj.word || ''}" required>
+      </div>
+      <div class="form-group">
+        <label for="definition">Definition</label>
+        <textarea class="form-control" placeholder="Definition" id="definition" style="height: 100px">${obj.definition || ''}</textarea>
+      </div>
+      <div class="form-group">
+        <label for="category">Category</label>
+        <input type="text" class="form-control" id="category" placeholder="Enter the Coding Language Associated with Your Word/Phrase" value="${obj.category || ''}" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit Book
+      </button>
 </form>`;
   renderToDom('#form-container', domString);
 };
