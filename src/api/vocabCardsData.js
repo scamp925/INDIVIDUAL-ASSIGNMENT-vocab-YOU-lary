567ed4df19f3bf6ec49ed5f169a17709faa3ef0b
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-07 21:51:17 -0500

    User is able to click 'edit btn' and a populated form renders. User is able to make changes to text, but can't submit. New bug

--- src/api/vocabCardsData.js
+++ src/api/vocabCardsData.js
@@ -12,6 +12,13 @@ const getVocabCards = (uid) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+// GET SINGLE VOCAB CARD
+const getSingleVocabCard = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/cards/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
 // CREATE VOCAB CARD
 const createVocabCard = (newVocabCardObj) => new Promise((resolve, reject) => {
   axios.post(`${dbUrl}/cards.json`, newVocabCardObj)
@@ -25,7 +32,10 @@ const createVocabCard = (newVocabCardObj) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+// UPDATE VOCAB CARD
+
 export {
   getVocabCards,
+  getSingleVocabCard,
   createVocabCard,
 };
