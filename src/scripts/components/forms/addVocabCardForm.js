32eecf57f40714bcd6b95821baf9adcfd60b1db7
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-11 11:47:30 -0500

    On the form, changed where 'category' selection look

--- src/scripts/components/forms/addVocabCardForm.js
+++ src/scripts/components/forms/addVocabCardForm.js
@@ -13,17 +13,16 @@ const addVocabCardForm = (obj = {}) => {
         <label for="definition">Definition</label>
         <textarea class="form-control" placeholder="Definition" id="definition" style="height: 100px" required>${obj.definition || ''}</textarea>
       </div>
-      <br>
-      <div class="form-floating mb-3">
-      <select class="form-select form-control-lg" id="category" aria-label="category" required>
-        <option value="">Select a category</option>
-        <option value="HTML">HTML</option>
-        <option value="CSS">CSS</option>
-        <option value="JavaScript">JavaScript</option>
-        <option value="React">React</option>
-        <option value="Python">Python</option>
-      </select>
       <label for="category">Category</label>
+      <div class="form-floating mb-3">
+      <select class="form-select form-control-lg" id="category" aria-label="category" ${obj.category || ''} required>
+      <option value="">Select a category</option>
+      <option value="HTML">HTML</option>
+      <option value="CSS">CSS</option>
+      <option value="JavaScript">JavaScript</option>
+      <option value="React">React</option>
+      <option value="Python">Python</option>
+      </select>
     </div>
       <button type="submit" class="btn btn-success">Submit</button>
 </form>`;
