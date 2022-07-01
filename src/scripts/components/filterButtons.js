d152c58a46d52030bc397582c5efea58e889cc62
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-01 18:58:41 -0500

    Added dropdown button for sorting to the filter button row. Need click events

--- src/scripts/components/filterButtons.js
+++ src/scripts/components/filterButtons.js
@@ -8,6 +8,12 @@ const filterBtns = () => {
   <button type="button" class="btn btn-warning" id="react">React</button>
   <button type="button" class="btn btn-warning" id="python">Python</button>
   <button type="button" class="btn btn-warning" id="all">All Languages</button>
+  <button class="btn btn-warning dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Sort Vocabulary Cards</button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <li><a class="dropdown-item">A-Z</a></li>
+      <li><a class="dropdown-item">Newest</a></li>
+      <li><a class="dropdown-item">Oldest</a></li>
+    </ul>
   </div>`;
   renderToDom('#filter-container', domString);
 };
