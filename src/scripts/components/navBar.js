e882821703f51e69ee5e0dee24d2fcf16c7f1012
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-05 15:05:09 -0500

    Removed old logo img and replace with a square one. Used border-radius to curve the logo

--- src/scripts/components/navBar.js
+++ src/scripts/components/navBar.js
@@ -1,11 +1,11 @@
 import renderToDom from '../helpers/renderToDom';
-import ABC from '../../images/ABC.png';
+import ABC from '../../images/ABC-square.png';
 
 const navBar = (user) => {
   const domString = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" id="home">
-      <img src=${ABC} alt="ABC blocks" width="100" height="50">
+      <img src=${ABC} alt="ABC blocks" width="50" height="50" style="border-radius: 50%;">
       Vocab-YOU-lary
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
